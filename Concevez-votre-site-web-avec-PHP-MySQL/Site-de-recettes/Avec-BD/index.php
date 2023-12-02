f80766dd5e53104b329aab48572afc6dd3a31c5e
Stephane Mouron <stephane.mouron@free.fr>
2023-12-02 18:45:46 +0100

    Mise en place d'un fichier .htaccess dans tous les projets et ajouts de balises meta dans les pages index

--- Concevez-votre-site-web-avec-PHP-MySQL/Site-de-recettes/Avec-BD/index.php
+++ Concevez-votre-site-web-avec-PHP-MySQL/Site-de-recettes/Avec-BD/index.php
@@ -7,6 +7,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Stephane Mouron">
+	<meta name="date.created" content="2022-09-17">
+	<meta name="date.updated" content="2023-12-02"> 
+    
+	<!-- Pour empêcher tous les moteurs de recherche compatibles -->
+	<meta name="robots" content="noindex">
+	<!-- Pour empêcher que les robots d'exploration Google d'indexer une page -->
+	<meta name="googlebot" content="noindex">
+
     <title>Site de recettes - Page d'accueil</title>
     <link
         href="./css/bootstrap.min.css" 
